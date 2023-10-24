1c9cec181e4c39a27d0ba29fdc12f43f44e71d62
gasimovv21 <gasimoweltun@gmail.com>
2023-10-24 02:02:35 +0200

    Added re_path in main urls.py for media and static files

--- carpartsazerbaijan/urls.py
+++ carpartsazerbaijan/urls.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+]
 
 
 if settings.DEBUG:
